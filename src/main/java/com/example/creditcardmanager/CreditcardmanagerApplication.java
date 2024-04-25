d74e274eb49ce617024d01333a231cba5833c114
Shruti Vasave <48850488+shrutivasave@users.noreply.github.com>
2024-04-25 11:03:33 -0400

    Initial commit

---
+++ src/main/java/com/example/creditcardmanager/CreditcardmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditcardmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardmanagerApplication.class, args);
+	}
+
+}
